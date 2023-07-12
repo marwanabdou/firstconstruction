f9d26cd0168efd48a332861dd3dc6a19d112d27b
Marwan <marwanhisham5@gmail.com>
2023-07-13 00:20:32 +0200

    adding bullet points to services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => (
     <p className="text-base md:text-lg md:text-left lg:text-justify">
       Our team has a wealth of knowledge and varied project experience. As a company, we offer the following services:
     </p>
-    <ul className="text-base md:text-lg mt-6 md:mt-8">
+    <ul className="text-base md:text-lg mt-6 md:mt-8 list-disc">
     <li className="mt-3 md:mt-5">Interior Design</li>
     <li className="mt-3 md:mt-5 ">Landscape Design</li>
     <li className="mt-3 md:mt-5 ">Remodeling</li>
