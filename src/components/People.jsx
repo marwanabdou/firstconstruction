5ba34088c4b3c5f313eb353c2572509d7b777539
Marwan <marwanhisham5@gmail.com>
2023-07-13 00:09:24 +0200

    removing svg from people component

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -77,28 +77,7 @@ const People = () => (
         </div>
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
           <span className="relative inline-block">
-            <svg
-              viewBox="0 0 52 24"
-              fill="currentColor"
-              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-            >
-              <defs>
-                <pattern
-                  id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
-                  x="0"
-                  y="0"
-                  width=".135"
-                  height=".30"
-                >
-                  <circle cx="1" cy="1" r=".7" />
-                </pattern>
-              </defs>
-              <rect
-                fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
-                width="52"
-                height="24"
-              />
-            </svg>
+           
             <span className="relative">Meet</span>
           </span>{' '}
           Our Leadership
